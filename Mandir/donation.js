8a081a0e139bd51c9b902ced4df85506c9d69978
Yesh Kumar <158145445+yesh7835@users.noreply.github.com>
2024-06-13 09:07:29 +0530

    Add files via upload

---
+++ Mandir/donation.js
@@ -0,0 +1,9 @@
+
+function toggleAnimation(element) {
+    element.classList.toggle('animate');
+}
+
+
+console.log(document.body.firstChild)
+console.log(document.body.lastChildChild)
+console.log(document.body.childNodes)
